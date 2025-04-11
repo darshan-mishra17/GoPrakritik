fdf5fdeb522ea33dd71190a7579498792df88168
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-04-12 02:34:16 +0530

    fixes

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -4,36 +4,21 @@ export default function Hero() {
   return (
     <div className="flex items-center md:mx-24 md:my-8 p-4">
       <div className="max-w-2xl">
-<<<<<<< HEAD
         <h1 className="text-white text-6xl font-bold mb-6 leading-[1.3]">
           Bringing Nature’s
-=======
-        <h1 className="text-white text-4xl md:text-6xl font-bold mb-6 leading-[1.3]">
-          Discover the Best
->>>>>>> a674be5aa9e0e63e0e027a3acdddcf11c8bef584
           <br />
           Best to Your
           <br />
           Home.
         </h1>
-<<<<<<< HEAD
         <p className="text-white/90 text-xl italic mb-10">
           Shop Organic. Live Pure. Feel the Difference.
-=======
-        <p className="text-white/90 text-lg md:text-xl italic mb-10">
-          Explore our wide range of organic products, including goat milk
-          and more!
->>>>>>> a674be5aa9e0e63e0e027a3acdddcf11c8bef584
         </p>
         <button className="group relative overflow-hidden bg-transparent text-lg md:text-xl text-white px-6 md:px-8 py-2 md:py-3 rounded-full font-semibold transition duration-500">
           <span className="absolute inset-0 border-2 border-white opacity-100 rounded-full transform scale-100 group-hover:scale-110 transition-transform duration-500"></span>
-<<<<<<< HEAD
 
           <span className="absolute inset-0 bg-green-600  transform translate-y-full group-hover:translate-y-0 transition-transform duration-150 ease-out rounded-full"></span>
 
-=======
-          <span className="absolute inset-0 bg-green-600 transform translate-y-full group-hover:translate-y-0 transition-transform duration-150 ease-out rounded-full"></span>
->>>>>>> a674be5aa9e0e63e0e027a3acdddcf11c8bef584
           <span className="relative z-10 transition duration-300 group-hover:text-white">
             Buy now
           </span>
